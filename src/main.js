5fe3ed006bab59d05190082052f06b1c494d75c1
Carlos Rodriguez <carlos.r@globant.com>
2017-09-26 18:23:38 +0100

    adding multiple windows

--- src/main.js
+++ src/main.js
@@ -6,25 +6,31 @@ const ipc = electron.ipcMain
 const app = electron.app
 const BrowserWindow = electron.BrowserWindow
 
-let mainWindow
+const windows = []
 
-app.on('ready', function(){
-	mainWindow = new BrowserWindow({
-		height: 400,
-		width: 400
+app.on('ready', function() {
+	[1,2,3].forEach(function() {
+		var win = new BrowserWindow({
+			height: 400,
+			width: 400
+		})
+		console.log("It's ready!")
+
+		win.loadURL(`file://${__dirname}/countdown.html`)
+
+		win.on('closed', function(){
+			console.log('closed')
+			win = null
+		})
+
+		windows.push(win)
 	})
-	console.log("It's ready!")
+})
 
-	mainWindow.loadURL(`file://${__dirname}/countdown.html`)
-
-	mainWindow.on('closed', function(){
-		console.log('closed')
-		mainWindow = null
-	})
-
-	ipc.on('countdown-start', function() {
-		countdown(function(count) {
-			mainWindow.webContents.send('countdown', count)
+ipc.on('countdown-start', function() {
+	countdown(function(count) {
+		windows.forEach(function(win) {
+			win.webContents.send('countdown', count)
 		})
 	})
 })
